d678aba86daa17042f602f601394d474a8dcc160
Stefan Zweifel <hello@stefanzweifel.io>
2017-03-21 21:26:21 +0100

    Update script

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-
-FILES=src/*.png
 FUZZ=20
 PRUNE=2
 SUFFIX="__processed.png";
 
-
-for f in $FILES; do
-
-    FILENAME=$(basename $f);
-    NEW_NAME=$FILENAME$SUFFIX;
+find ./src -type f -name '*.png' -print0 | while IFS= read -r -d '' file; do
+    FILENAME=$(basename "$file")
 
     echo "Processing $FILENAME file...";
 
-    ./bin/multicrop -c SouthWest -f $FUZZ -p $PRUNE $f dist/$FILENAME$SUFFIX
+    ./bin/multicrop -c 10,10 -d 25 -f $FUZZ -p $PRUNE "$file" dist/$FILENAME$SUFFIX >> /dev/null 2>&1
+
+    # Run with -c SouthWest when the lower left corner is more white than the top left corner
+    # -c SouthWest
+
 done
